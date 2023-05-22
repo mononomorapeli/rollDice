9a061afdf92bc0ebb857183cf714cfe92ac3f97c
mononomorapeli <minkymorapeli@gmail.com>
2023-05-22 11:17:37 +0200

    gg

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        diceOne.image = UIImage(named:"5")
+        diceTwo.image = #imageLiteral(resourceName: "6")
+        
+        let diceOne = arc4random_uniform(6) + 1
+          let diceTWo = arc4random_uniform(6) + 1
+  
+        
+   
+    }
+   
 }
 
